a48088d62cfe721cc08c701b14b5ad0a934cf983
Mat Brown <mat@patch.com>
2008-11-07 19:52:32 -0500

    Upgraded generated files
    Upgraded Rakefile to a new one generated by the latest newgem. Also
    added version to lib/sunspot.rb

--- lib/sunspot.rb
+++ lib/sunspot.rb
@@ -7,4 +7,5 @@ require 'extlib'
 %w(condition conditions scope_builder field field_builder indexer query query_builder search searchable type).each { |filename| require File.join(File.dirname(__FILE__), 'sunspot', filename) }
 
 module Sunspot
+  VERSION='0.0.1'
 end
