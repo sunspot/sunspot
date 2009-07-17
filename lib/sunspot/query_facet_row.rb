3dd0c9c5dab795d839cd119ea77b014b8a6eb1f3
Mat Brown <mat@patch.com>
2009-07-17 17:00:05 -0400

    Make QueryFacetRow a real class (mostly for docs)

--- lib/sunspot/query_facet_row.rb
+++ lib/sunspot/query_facet_row.rb
@@ -1,3 +1,9 @@
 module Sunspot
-  QueryFacetRow = Struct.new(:value, :count)
+  class QueryFacetRow
+    attr_reader :value, :count
+
+    def initialize(value, count)
+      @value, @count = value, count
+    end
+  end
 end
