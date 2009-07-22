20b726fabe90218161a902694b75012a895e7f42
Mat Brown <mat@patch.com>
2009-07-22 16:50:45 -0400

    Add sunspot:solr:run rake task

--- lib/sunspot/rails/tasks.rb
+++ lib/sunspot/rails/tasks.rb
@@ -21,6 +21,22 @@ namespace :sunspot do
       end
     end
 
+    desc 'Run the Solr instance in the foreground. Windows users must use this task to run Solr'
+    task :run => :environment do
+      data_path = File.join(::Rails.root, 'solr', 'data', ::Rails.env)
+      solr_home =
+        if %w(solrconfig schema).all? { |file| File.exist?(File.join(::Rails.root, 'solr', 'conf', "#{file}.xml")) }
+          File.join(::Rails.root, 'solr')
+        end
+      FileUtils.mkdir_p(data_path)
+      port = Sunspot::Rails.configuration.port
+      command = ['sunspot-solr', 'run', '--', '-p', port.to_s, '-d', data_path]
+      if solr_home
+        command << '-s' << solr_home
+      end
+      exec(Escape.shell_command(command))
+    end
+
     desc 'Stop the Solr instance'
     task :stop => :environment do
       FileUtils.cd(File.join(::Rails.root, 'solr', 'pids', ::Rails.env)) do
