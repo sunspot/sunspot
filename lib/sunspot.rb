978c6559a572909a29a147c54e1c272a1c7c98c2
Steven Soroka <ssoroka78@gmail.com>
2009-09-10 19:59:55 -0500

    move sunspot requires to proper sunspot/rails.rb file, load before Sunspot::Rails::Configuration is needed

--- lib/sunspot.rb
+++
@@ -1,9 +0,0 @@
-require 'sunspot/rails/configuration'
-require 'sunspot/rails/adapters'
-require 'sunspot/rails/request_lifecycle'
-require 'sunspot/rails/searchable'
-require 'sunspot/rails/tasks'
-require 'sunspot/rails'
-
-module Sunspot
-end
