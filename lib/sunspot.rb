2fd60fc3f983533cc7f4e8abebe314dacfe6ab87
Steven Soroka <ssoroka78@gmail.com>
2009-09-10 19:51:43 -0500

    make some changes to have this run nicer as a gem.

---
+++ lib/sunspot.rb
@@ -0,0 +1,9 @@
+require 'sunspot/rails/configuration'
+require 'sunspot/rails/adapters'
+require 'sunspot/rails/request_lifecycle'
+require 'sunspot/rails/searchable'
+require 'sunspot/rails/tasks'
+require 'sunspot/rails'
+
+module Sunspot
+end
