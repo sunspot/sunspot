14fb35b6420550f019f11be43d0838e42cb5b54c
Mat Brown <mat@patch.com>
2009-09-25 09:46:36 -0400

    Reverse location of schema.rb

--- spec/mock_app/db/schema.rb
+++ spec/mock_app/db/schema.rb
@@ -1,14 +1,1 @@
-ActiveRecord::Schema.define(:version => 0) do
-  create_table :posts, :force => true do |t|
-    t.string :title
-    t.text :body
-    t.references :blog
-    t.timestamps
-  end
-
-  create_table :blogs, :force => true do |t|
-    t.string :name
-    t.string :subdomain
-    t.timestamps
-  end
-end
+../../schema.rb
--- spec/schema.rb
+++ spec/schema.rb
@@ -1,1 +1,14 @@
-mock_app/db/schema.rb
+ActiveRecord::Schema.define(:version => 0) do
+  create_table :posts, :force => true do |t|
+    t.string :title
+    t.text :body
+    t.references :blog
+    t.timestamps
+  end
+
+  create_table :blogs, :force => true do |t|
+    t.string :name
+    t.string :subdomain
+    t.timestamps
+  end
+end
