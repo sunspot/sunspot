35318c80c75bcb50b9b7177f8257a2a69c24bd49
Benjamin Krause <benjamin@totoro.local>
2009-10-21 09:29:36 +0200

    added require rubygems to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] = 'test'
 ENV['RAILS_ROOT'] ||= File.join(File.dirname(__FILE__), 'mock_app')
 
+require 'rubygems'
 gem 'sunspot'
 
 require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config', 'environment.rb'))
