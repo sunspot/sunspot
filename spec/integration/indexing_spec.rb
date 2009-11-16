22a1c1062db2231598e658f1167de7e01b554cf8
Tom Coleman <tom@thesnail.org>
2009-11-16 11:28:50 +1100

    Added regression test for singular fields with newlines

---
+++ spec/integration/indexing_spec.rb
@@ -0,0 +1,7 @@
+describe 'indexing' do
+  it 'should index non-multivalued field with newlines' do
+    lambda do
+      Sunspot.index!(Post.new :title => "A\nTitle")
+    end.should_not raise_error RSolr::RequestError
+  end
+end
