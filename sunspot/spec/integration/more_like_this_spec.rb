9704a568460e1fa2470d868ed40e4a0f803e1025
Mat Brown <mat@patch.com>
2010-03-23 14:15:11 -0400

    Refactored MLT integration spec a bit

--- sunspot/spec/integration/more_like_this_spec.rb
+++ sunspot/spec/integration/more_like_this_spec.rb
@@ -2,21 +2,22 @@ describe 'more_like_this' do
   before :all do
     Sunspot.remove_all
     @posts = [
-      Post.new(:title => 'Post123', :body => "one two three", :tags => %w(ruby sunspot rsolr)),
-      Post.new(:title => 'Post456', :body => "four five six", :tags => %w(ruby solr lucene)),
-      Post.new(:title => 'Post234', :body => "two three four", :tags => %w(python sqlalchemy)),
-      Post.new(:title => 'Post345', :body => "three four five", :tags => %w(ruby sunspot mat)),
+      Post.new(:body => "one two three", :tags => %w(ruby sunspot rsolr)),
+      Post.new(:body => "four five six", :tags => %w(ruby solr lucene)),
+      Post.new(:body => "two three four", :tags => %w(python sqlalchemy)),
+      Post.new(:body => "three four five", :tags => %w(ruby sunspot mat)),
+      Post.new(:body => "six seven eight", :tags => %w(bogus airplane))
     ]
     Sunspot.index!(@posts)
   end
 
-  it 'should return results' do
-    Sunspot.more_like_this(@posts.first).results.should_not be_empty
+  it 'should return results for all MLT fields' do
+    Sunspot.more_like_this(@posts.first).results.to_set.should == @posts[1..3].to_set
   end
 
-  it 'should return results for text fields' do
+  it 'should return results for specified text field' do
     Sunspot.more_like_this(@posts.first) do 
       fields :body
-    end.results.should == @posts[2..3]
+    end.results.to_set.should == @posts[2..3].to_set
   end
 end
