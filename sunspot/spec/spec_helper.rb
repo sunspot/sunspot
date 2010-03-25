206e4792b2386a45151bf51aa1296383fff6c40b
Mat Brown <mat@patch.com>
2010-03-25 11:30:54 -0400

    Simply test gem dependencies

--- sunspot/spec/spec_helper.rb
+++ sunspot/spec/spec_helper.rb
@@ -1,5 +1,3 @@
-using_gems = false
-
 if rsolr_gem_version = ENV['RSOLR_GEM_VERSION']
   STDERR.puts("Forcing rsolr gem version #{rsolr_gem_version}")
   using_gems = true
@@ -7,35 +5,20 @@ if rsolr_gem_version = ENV['RSOLR_GEM_VERSION']
   gem 'rsolr', rsolr_gem_version
 end
 
+require 'ostruct'
 begin
   require 'spec'
-  begin
-    require 'ruby-debug'
-  rescue LoadError => e
-    if using_gems
-      module Kernel
-        def debugger
-          STDERR.puts('Debugger is not available')
-        end
-      end
-    else
-      raise(e)
-    end
-  end
   if ENV['USE_WILL_PAGINATE']
     require 'will_paginate'
     require 'will_paginate/collection'
   end
 rescue LoadError => e
-  require 'rubygems'
-  if using_gems
-    raise(e)
-  else
-    using_gems = true
+  if require 'rubygems'
     retry
+  else
+    raise(e)
   end
 end
-require 'ostruct'
 
 unless gem_name = ENV['SUNSPOT_TEST_GEM']
   $:.unshift(File.dirname(__FILE__) + '/../lib')
