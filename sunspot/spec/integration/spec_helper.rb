1af35e65c3b4cef3a617f4b35cd080c824092d9d
Vít Ondruch <vondruch@redhat.com>
2011-03-10 16:47:02 +0100

    Custom SOLR url has to be setup before anything else is called.

--- sunspot/spec/integration/spec_helper.rb
+++ sunspot/spec/integration/spec_helper.rb
@@ -1,7 +1,7 @@
 require File.expand_path('spec_helper', File.join(File.dirname(__FILE__), '..'))
 
 Spec::Runner.configure do |config|
-  config.before(:each) do
+  config.before(:all) do
     Sunspot.config.solr.url = ENV['SOLR_URL'] || 'http://localhost:8983/solr'
   end
 end
