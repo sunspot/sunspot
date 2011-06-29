5313dbff3b93a4f4da07ca02445169b3d97a872b
Larry Sprock <larry@lucidbleu.com>
2011-06-29 07:51:32 -0700

    Specify activesupport/to_json load order to fix to_json bug

--- sunspot_rails/lib/sunspot_rails.rb
+++ sunspot_rails/lib/sunspot_rails.rb
@@ -1,3 +1,8 @@
+# This needs to be loaded before sunspot/search/paginated_collection
+# or #to_json gets defined in Object breaking delegation to Array via
+# method_missing
+require 'active_support/core_ext/object/to_json' if Rails::VERSION::MAJOR == 3
+
 require 'sunspot/rails'
 
 if Rails::VERSION::MAJOR == 3
