0dc0b626f059f2f8a741c0c8006e0ba52332835d
Nick Zadrozny <nick@beyondthepath.com>
2011-08-17 02:52:40 -0700

    Back to the noisy solr startup, and tweak some Gemfile syntax

--- ci/travis.sh
+++ ci/travis.sh
@@ -4,14 +4,14 @@ case $GEM in
   "sunspot")
     cd sunspot
     bundle install
-    bundle exec sunspot-solr start -p 8983
+    bundle exec sunspot-solr run -p 8983 &
     sleep 5
     bundle exec rake spec
     ;;
   "sunspot_rails")
     cd sunspot
     bundle install
-    bundle exec sunspot-solr start -p 8980
+    bundle exec sunspot-solr run -p 8980 &
     sleep 5
     cd ../sunspot_rails/spec/$RAILS
     bundle install
