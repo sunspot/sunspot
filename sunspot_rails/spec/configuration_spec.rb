08f0b557e75985956476c027186d1a606d7e61f8
Nick Zadrozny <nick@beyondthepath.com>
2011-09-05 09:01:22 -0700

    Different idiom for requiring relative files that works better across 1.8 and 1.9

--- sunspot_rails/spec/configuration_spec.rb
+++ sunspot_rails/spec/configuration_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.expand_path('spec_helper', File.dirname(__FILE__))
 
 describe Sunspot::Rails::Configuration, "default values without a sunspot.yml" do
   before(:each) do
--- sunspot_rails/spec/model_lifecycle_spec.rb
+++ sunspot_rails/spec/model_lifecycle_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.expand_path('spec_helper', File.dirname(__FILE__))
 
 describe 'searchable with lifecycle' do
   describe 'on create' do
