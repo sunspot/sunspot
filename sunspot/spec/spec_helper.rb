581bf551272a746386d3c74e86deaed1739c9286
Andy Lindeman <andy@highgroove.com>
2011-09-16 23:26:48 -0400

    Update to RSpec 2.6 and refactor test helpers along the way

--- sunspot/spec/spec_helper.rb
+++ sunspot/spec/spec_helper.rb
@@ -7,17 +7,28 @@ require File.join(File.dirname(__FILE__), 'mocks', 'mock_record.rb')
 Dir.glob(File.join(File.dirname(__FILE__), 'mocks', '**', '*.rb')).each do |file|
   require file unless File.basename(file) == 'mock_record.rb'
 end
+Dir.glob(File.join(File.dirname(__FILE__), "helpers", "*.rb")).each do |file|
+  require file
+end
 require File.join(File.dirname(__FILE__), 'ext')
 
-Spec::Runner.configure do |config|
-  Dir.glob(File.join(File.dirname(__FILE__), 'helpers', '*_helper.rb')).each do |helper|
-    helper_name = File.basename(helper, File.extname(helper))
-    spec_type = helper_name.sub(/_helper$/, '').to_sym
-    require(helper)
-    config.include(
-      Sunspot::Util.full_const_get(Sunspot::Util.camel_case(helper_name)),
-      :type => spec_type
-    )
+RSpec.configure do |config|
+  config.before(:each, :type => :integration) do
+    Sunspot.config.solr.url = ENV['SOLR_URL'] || 'http://localhost:8983/solr'
+  end
+
+  # Mock session available to all spec/api tests
+  config.include MockSessionHelper,
+                 :type => :api,
+                 :example_group => {:file_path => /spec[\\\/]api/}
+
+  # Nested under spec/api
+  [:indexer, :query, :search].each do |spec_type|
+    helper_name = "#{spec_type}_helper"
+
+    config.include Sunspot::Util.full_const_get(Sunspot::Util.camel_case(helper_name)),
+                   :type          => spec_type,
+                   :example_group => {:file_path => /spec[\\\/]api[\\\/]#{spec_type}/}
   end
 end
 
