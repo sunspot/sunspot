e5dca12e3c7957a6832325ff99470c6ca5b3dfc6
Bruno Miranda <bruno@bopia.com>
2011-09-28 14:33:45 -0300

    Very basic spec to test spatial search.

--- sunspot/spec/api/query/standard_spec.rb
+++ sunspot/spec/api/query/standard_spec.rb
@@ -11,6 +11,7 @@ describe 'standard query', :type => :query do
   it_should_behave_like "sortable query"
   it_should_behave_like "query with text field scoping"
   it_should_behave_like "geohash query"
+  it_should_behave_like "spatial query"
 
   it 'adds a no-op query to :q parameter when no :q provided' do
     session.search Post do
