17abbe7d7dd38cae13958c11c0399eaeaa743420
David Beckwith <thirdreplicator@gmail.com>
2011-09-29 07:13:52 -0700

    Added a disable_solr to sunspot.yml.  Disables Solr.

--- sunspot_rails/spec/session_spec.rb
+++ sunspot_rails/spec/session_spec.rb
@@ -10,6 +10,32 @@ describe 'Sunspot::Rails session' do
   it 'should not create a separate master/slave session if no master configured' do
   end
 
+  it 'should do nothing to the session if disable_solr: false (default) ' do
+    @config = Sunspot::Rails::Configuration.new
+    @config.disable_solr?.should == false
+    Sunspot.session.class.should == Sunspot::SessionProxy::ThreadLocalSessionProxy
+  end
+
+  describe 'disable_solr' do
+    before(:each) do
+      ::Rails.stub!(:env => 'disabled')
+      @config = Sunspot::Rails::Configuration.new
+    end
+
+    after(:each) do
+      ::Rails.stub!(:env => 'config_test')
+      if Sunspot.session.class == Sunspot::Rails::StubSessionProxy
+        Sunspot.session = Sunspot.session.original_session
+      end
+    end
+
+    it 'should stub the session if disable_solr: true' do
+      @config.disable_solr?.should == true
+      Sunspot.session.class.should == Sunspot::Rails::StubSessionProxy
+      Sunspot.session = Sunspot.session.original_session
+    end
+  end
+
   private
 
   def with_configuration(options)
