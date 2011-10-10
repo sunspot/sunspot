79e38e7929b0b48deae09460f81a52d08ea74ae6
Andy Lindeman <andy@highgroove.com>
2011-10-09 20:16:04 -0400

    Wait a bit more smartly for Solr to start in Travis script

--- ci/travis.sh
+++ ci/travis.sh
@@ -2,15 +2,29 @@
 
 set -e
 
+solr_responding() {
+  port=$1
+  curl -o /dev/null "http://localhost:$port/solr" > /dev/null 2>&1
+}
+
+wait_until_solr_responds() {
+  port=$1
+  while ! solr_responding $1; do
+    /bin/echo -n "."
+    sleep 1
+  done
+}
+
 case $GEM in
   "sunspot")
     
     cd sunspot
-    /bin/echo -n "Starting Solr on port 8983 for Sunspot specs... "
+    /bin/echo -n "Starting Solr on port 8983 for Sunspot specs..."
     bundle install --quiet --path vendor/bundle
     if [ -f sunspot-solr.pid ]; then bundle exec sunspot-solr stop || true; fi
+
     bundle exec sunspot-solr start -p 8983
-    sleep 10
+    wait_until_solr_responds 8983
     /bin/echo "done."
     
     # Invoke the sunspot specs
@@ -24,11 +38,12 @@ case $GEM in
   "sunspot_rails")
   
     cd sunspot
-    /bin/echo -n "Starting Solr on port 8980 for Sunspot specs... "
+    /bin/echo -n "Starting Solr on port 8980 for Sunspot specs..."
     bundle install --quiet --path vendor/bundle
     if [ -f sunspot-solr.pid ]; then bundle exec sunspot-solr stop || true; fi
+
     bundle exec sunspot-solr start -p 8980
-    sleep 10
+    wait_until_solr_responds 8980
     /bin/echo "done."
     
     # Install gems for test Rails application
