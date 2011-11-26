b8be273d04d255f1eac2fffd8e1cdc6c1eb9511d
Andy Lindeman <andy@highgroove.com>
2011-11-26 14:27:45 -0600

    Debugging travis build

--- sunspot/spec/helpers/integration_helper.rb
+++ sunspot/spec/helpers/integration_helper.rb
@@ -2,6 +2,7 @@ module IntegrationHelper
   def self.included(base)
     base.before do
       Sunspot.config.solr.url = ENV['SOLR_URL'] || 'http://localhost:8983/solr'
+      puts "Expecting Solr to be running at #{Sunspot.config.solr.url}"
     end
   end
 end
