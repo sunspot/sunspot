558d7902ad4113e181726c54c63ff42b8b7bf9a4
Andy Lindeman <andy@highgroove.com>
2011-11-26 15:47:16 -0600

    Creates a new session before each integration spec

--- sunspot/spec/helpers/integration_helper.rb
+++ sunspot/spec/helpers/integration_helper.rb
@@ -1,8 +1,8 @@
 module IntegrationHelper
   def self.included(base)
     base.before do
-      Sunspot.reset!
       Sunspot.config.solr.url = ENV['SOLR_URL'] || 'http://localhost:8983/solr'
+      Sunspot.reset!(true)
     end
   end
 end
