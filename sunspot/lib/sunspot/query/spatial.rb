03a1a28e2da6eb527c7bed555c4547629026bb83
Andy Lindeman <andy@highgroove.com>
2011-12-10 16:12:37 -0500

    Fixes tests

--- sunspot/lib/sunspot/query/spatial.rb
+++ sunspot/lib/sunspot/query/spatial.rb
@@ -5,10 +5,16 @@ module Sunspot
         @field, @lat, @lon, @options = field, lat, lon, options
       end
 
+      # TODO: I don't necessarily think sorting by geodist() should be
+      # the default. Let's pull this out and support something like 
+      # order_by(:geodist)
       def to_params
-        params = {:sfield => @field.indexed_name, :fq => "{!geofilt}", :pt => "#{@lat},#{@lon}", :sort => "geodist() asc"}
-        params[:d] = @options[:radius] if @options[:radius]
-        params
+        {:sfield => @field.indexed_name,
+         :fq     => "{!geofilt}",
+         :pt     => "#{@lat},#{@lon}",
+         :sort   => "geodist() asc"}.tap do |params|
+          params[:d] = @options[:radius] if @options[:radius]
+        end
       end
 
     end
