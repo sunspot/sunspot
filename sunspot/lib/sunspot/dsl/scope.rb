bd8399e2a37910e2c5b0ba1ea4c84cab4efa30f3
Ben Woosley <ben.woosley@gmail.com>
2012-03-29 00:13:02 -0700

    Do the field setup once

--- sunspot/lib/sunspot/dsl/scope.rb
+++ sunspot/lib/sunspot/dsl/scope.rb
@@ -191,12 +191,12 @@ module Sunspot
         case args.first
         when String, Symbol
           raise ArgumentError if args.length > 2
-          field_name = args[0]
+          field = @setup.field(args[0].to_sym)
           if args.length > 1
             value = args[1]
-            @scope.add_shorthand_restriction(negated, @setup.field(field_name.to_sym), value)
+            @scope.add_shorthand_restriction(negated, field, value)
           else # NONE
-            DSL::Restriction.new(@setup.field(field_name.to_sym), @scope, negated)
+            DSL::Restriction.new(field, @scope, negated)
           end
         else # args are instances
           @scope.add_restriction(
