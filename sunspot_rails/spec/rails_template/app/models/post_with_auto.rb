3b6981fbbdc9dbcd6c3f9f4699ca5553f554f5fc
vanstee <vanstee@highgroove.com>
2012-06-14 16:26:37 -0400

    Make attributes attr_accessible in rails_template
    Running the tests against rails 2.3.6 was blowing up do to some mass
    assignment issues. Fixed it by making all attributes attr_accessible.

--- sunspot_rails/spec/rails_template/app/models/post_with_auto.rb
+++ sunspot_rails/spec/rails_template/app/models/post_with_auto.rb
@@ -3,6 +3,8 @@ class PostWithAuto < ActiveRecord::Base
     'posts'
   end
 
+  attr_accessible :title, :type, :location_id, :body, :blog
+
   searchable :ignore_attribute_changes_of => [ :updated_at ] do
     string :title
     text :body, :more_like_this => true
