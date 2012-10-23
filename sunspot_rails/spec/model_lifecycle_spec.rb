9f04d34203a854b96ae7bbd60ae3a00274d7c048
Carol Nichols <carol.nichols@gmail.com>
2012-10-22 20:19:52 -0400

    Move a 'describe' beneath the main 'describe' to eliminate duplication in the desc value

--- sunspot_rails/spec/model_lifecycle_spec.rb
+++ sunspot_rails/spec/model_lifecycle_spec.rb
@@ -37,7 +37,7 @@ describe 'searchable with lifecycle' do
       @post.save!
     end
   end
-  
+
   describe 'on destroy' do
     before :each do
       @post = PostWithAuto.create
@@ -49,15 +49,16 @@ describe 'searchable with lifecycle' do
       PostWithAuto.search_ids.should be_empty
     end
   end
+
+  describe 'ignoring specific attributes' do
+    before(:each) do
+      @post = PostWithAuto.create
+    end
+
+    it "should not reindex the object on an update_at change, because it is marked as to-ignore" do
+      Sunspot.should_not_receive(:index).with(@post)
+      @post.update_attribute :updated_at, 123.seconds.from_now
+    end
+  end
 end
 
-describe 'searchable with lifecycle - ignoring specific attributes' do
-  before(:each) do
-    @post = PostWithAuto.create
-  end
-  
-  it "should not reindex the object on an update_at change, because it is marked as to-ignore" do
-    Sunspot.should_not_receive(:index).with(@post)
-    @post.update_attribute :updated_at, 123.seconds.from_now
-  end
-end
