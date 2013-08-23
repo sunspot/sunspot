299d5d941cd75ecc3c55826edd43473ac9f30bed
peretzp <peretzp@gmail.com>
2013-08-22 20:47:47 -0700

    strip trailing whitespace

--- sunspot/lib/sunspot/session.rb
+++ sunspot/lib/sunspot/session.rb
@@ -1,5 +1,5 @@
 module Sunspot
-  # 
+  #
   # A Sunspot session encapsulates a connection to Solr and a set of
   # configuration choices. Though users of Sunspot may manually instantiate
   # Session objects, in the general case it's easier to use the singleton
@@ -10,8 +10,8 @@ module Sunspot
   class Session
     class <<self
       attr_writer :connection_class #:nodoc:
-      
-      # 
+
+      #
       # For testing purposes
       #
       def connection_class #:nodoc:
@@ -19,12 +19,12 @@ module Sunspot
       end
     end
 
-    # 
+    #
     # Sunspot::Configuration object for this session
     #
     attr_reader :config
 
-    # 
+    #
     # Sessions are initialized with a Sunspot configuration and a Solr
     # connection. Usually you will want to stick with the default arguments
     # when instantiating your own sessions.
@@ -36,7 +36,7 @@ module Sunspot
       @deletes = @adds = 0
     end
 
-    # 
+    #
     # See Sunspot.new_search
     #
     def new_search(*types, &block)
@@ -59,7 +59,7 @@ module Sunspot
       search.execute
     end
 
-    # 
+    #
     # See Sunspot.new_more_like_this
     #
     def new_more_like_this(object, *types, &block)
@@ -91,7 +91,7 @@ module Sunspot
       indexer.add(objects)
     end
 
-    # 
+    #
     # See Sunspot.index!
     #
     def index!(*objects)
@@ -115,7 +115,7 @@ module Sunspot
       connection.optimize
     end
 
-    # 
+    #
     # See Sunspot.remove
     #
     def remove(*objects, &block)
@@ -139,7 +139,7 @@ module Sunspot
       end
     end
 
-    # 
+    #
     # See Sunspot.remove!
     #
     def remove!(*objects)
@@ -147,7 +147,7 @@ module Sunspot
       commit
     end
 
-    # 
+    #
     # See Sunspot.remove_by_id
     #
     def remove_by_id(clazz, id)
@@ -160,7 +160,7 @@ module Sunspot
       indexer.remove_by_id(class_name, id)
     end
 
-    # 
+    #
     # See Sunspot.remove_by_id!
     #
     def remove_by_id!(clazz, id)
@@ -182,7 +182,7 @@ module Sunspot
       end
     end
 
-    # 
+    #
     # See Sunspot.remove_all!
     #
     def remove_all!(*classes)
@@ -190,35 +190,35 @@ module Sunspot
       commit
     end
 
-    # 
+    #
     # See Sunspot.dirty?
     #
     def dirty?
       (@deletes + @adds) > 0
     end
 
-    # 
+    #
     # See Sunspot.commit_if_dirty
     #
     def commit_if_dirty
       commit if dirty?
     end
-    
-    # 
+
+    #
     # See Sunspot.delete_dirty?
     #
     def delete_dirty?
       @deletes > 0
     end
 
-    # 
+    #
     # See Sunspot.commit_if_delete_dirty
     #
     def commit_if_delete_dirty
       commit if delete_dirty?
     end
-    
-    # 
+
+    #
     # See Sunspot.batch
     #
     def batch
@@ -229,7 +229,7 @@ module Sunspot
 
     private
 
-    # 
+    #
     # Retrieve the Solr connection for this session, creating one if it does not
     # already exist.
     #
--- sunspot/lib/sunspot/session_proxy/id_sharding_session_proxy.rb
+++ sunspot/lib/sunspot/session_proxy/id_sharding_session_proxy.rb
@@ -1,6 +1,6 @@
 module Sunspot
   module SessionProxy
-    # 
+    #
     # A concrete implementation of ShardingSessionProxy that determines the
     # shard for a given object based on the hash of its class and ID.
     #
@@ -24,7 +24,7 @@ module Sunspot
       attr_reader :sessions
       alias_method :all_sessions, :sessions #:nodoc:
 
-      # 
+      #
       # Initialize with a search session (see ShardingSessionProxy.new) and a
       # collection of one or more shard sessions. See note about changing the
       # number of shard sessions in the documentation for this class.
@@ -34,7 +34,7 @@ module Sunspot
         @sessions = shard_sessions
       end
 
-      # 
+      #
       # Return a session based on the hash of the class and ID, modulo the
       # number of shard sessions.
       #
@@ -42,7 +42,7 @@ module Sunspot
         session_for_index_id(Adapters::InstanceAdapter.adapt(object).index_id)
       end
 
-      # 
+      #
       # See Sunspot.remove_by_id
       #
       def remove_by_id(clazz, id)
@@ -51,7 +51,7 @@ module Sunspot
         ).remove_by_id(clazz, id)
       end
 
-      # 
+      #
       # See Sunspot.remove_by_id!
       #
       def remove_by_id!(clazz, id)
@@ -66,7 +66,7 @@ module Sunspot
         @sessions[id_hash(index_id) % @sessions.length]
       end
 
-      # 
+      #
       # This method is implemented explicitly instead of using String#hash to
       # give predictable behavior across different Ruby interpreters.
       #
