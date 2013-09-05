5427f1ff75b8d8595d9cc9e3a677b29b19a9e245
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2013-09-05 16:47:14 -0700

    Allow expected Travis failures because the CI suite results are useless because they always return a failure, even when desired specs pass. Removed deprecation warning from test suite.

--- sunspot_rails/spec/rails_template/app/models/post_with_default_scope.rb
+++ sunspot_rails/spec/rails_template/app/models/post_with_default_scope.rb
@@ -5,7 +5,9 @@ class PostWithDefaultScope < ActiveRecord::Base
 
   attr_accessible :title, :type, :location_id, :body, :blog
 
-  default_scope :order => :title # Don't worry about Rails 4.0 deprecation warning as this is also used in Rails 3.x tests
+	def self.default_scope
+		order(:title)
+  end
 
   searchable :auto_index => false, :auto_remove => false do
     string :title
