b9f07af8cec8ad2450d8dc92ca58b50eb3d49165
Christophe Maximin <christophe.maximin@gmail.com>
2013-12-24 04:34:53 -0400

    Fixed deprecated (by ActiveSupport 4.1.0) require of "active_support/core_ext/object/to_json"

--- sunspot_rails/lib/sunspot_rails.rb
+++ sunspot_rails/lib/sunspot_rails.rb
@@ -1,7 +1,11 @@
 # This needs to be loaded before sunspot/search/paginated_collection
 # or #to_json gets defined in Object breaking delegation to Array via
 # method_missing
-require 'active_support/core_ext/object/to_json'
+if ::Rails.version >= '4.1'
+  require 'active_support/core_ext/object/json'
+elsif ::Rails.version >= '3'
+  require 'active_support/core_ext/object/to_json'
+end
 
 require 'sunspot/rails'
 require 'sunspot/rails/railtie'
