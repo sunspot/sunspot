4ed3bd3823100ed010164684dbc41294d6a0ff16
Hamed Asghari <hasghari@g2crowd.com>
2014-03-07 21:19:16 -0500

    fix deprecation warning

--- sunspot_rails/lib/sunspot_rails.rb
+++ sunspot_rails/lib/sunspot_rails.rb
@@ -1,7 +1,7 @@
 # This needs to be loaded before sunspot/search/paginated_collection
 # or #to_json gets defined in Object breaking delegation to Array via
 # method_missing
-require 'active_support/core_ext/object/to_json'
+require 'active_support/core_ext/object/json'
 
 require 'sunspot/rails'
 require 'sunspot/rails/railtie'
