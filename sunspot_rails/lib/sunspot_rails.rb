5c868ea37877b1006a838171352ced3d0854571d
Zach Church <zgchurch@gmail.com>
2014-03-30 10:35:17 -0700

    Load ActiveSupport JSON support correctly for major Rails versions > 4

--- sunspot_rails/lib/sunspot_rails.rb
+++ sunspot_rails/lib/sunspot_rails.rb
@@ -1,7 +1,7 @@
 # This needs to be loaded before sunspot/search/paginated_collection
 # or #to_json gets defined in Object breaking delegation to Array via
 # method_missing
-if Rails::VERSION::MAJOR >= 4 && Rails::VERSION::MINOR >= 1
+if Rails::VERSION::MAJOR > 4 || (Rails::VERSION::MAJOR == 4 && Rails::VERSION::MINOR >= 1)
   require 'active_support/core_ext/object/json'
 else
   require 'active_support/core_ext/object/to_json'
