372db4146acfb87ea8363b6a112718d81fb89603
Stephen Pike <spike@scpike.net>
2014-06-16 18:43:19 -0400

    Spellcheck support
    This adds support for setting the spellcheck parameter through the
    query DSL with `spellcheck`, and for parsing the response. See the
    README for more.

--- sunspot/lib/sunspot/search/standard_search.rb
+++ sunspot/lib/sunspot/search/standard_search.rb
@@ -1,6 +1,6 @@
 module Sunspot
   module Search
-    # 
+    #
     # This class encapsulates the results of a Solr search. It provides access
     # to search results, total result count, facets, and pagination information.
     # Instances of Search are returned by the Sunspot.search and
@@ -10,22 +10,24 @@ module Sunspot
       def request_handler
         super || :select
       end
-      
+
       # Return the raw spellcheck block from the Solr response
       def solr_spellcheck
         @solr_spellcheck ||= @solr_result['spellcheck'] || {}
       end
-      
+
       # Reformat the oddly-formatted spellcheck suggestion array into a
       # more useful hash.
       #
       # Original: [term, suggestion, term, suggestion, ..., "correctlySpelled", bool, "collation", str]
+      #           "collation" is only included if spellcheck.collation was set to true
       # Returns: { term => suggestion, term => suggestion }
       def spellcheck_suggestions
         unless defined?(@spellcheck_suggestions)
           @spellcheck_suggestions = {}
-          count = (solr_spellcheck['suggestions'].length - 4) / 2
+          count = ((solr_spellcheck['suggestions'] || []).length) / 2
           (0..(count - 1)).each do |i|
+            break if ["correctlySpelled", "collation"].include? solr_spellcheck[i]
             term = solr_spellcheck['suggestions'][i * 2]
             suggestion = solr_spellcheck['suggestions'][(i * 2) + 1]
             @spellcheck_suggestions[term] = suggestion
@@ -33,7 +35,7 @@ module Sunspot
         end
         @spellcheck_suggestions
       end
-      
+
       # Return the suggestion with the single highest frequency.
       # Requires the extended results format.
       def spellcheck_suggestion_for(term)
@@ -41,7 +43,7 @@ module Sunspot
           suggestion['freq']
         end.last['word']
       end
-      
+
       # Provide a collated query. If the user provides a query string,
       # tokenize it on whitespace and replace terms strictly not present in
       # the index. Otherwise return Solr's suggested collation.
@@ -54,31 +56,31 @@ module Sunspot
       def spellcheck_collation(*terms)
         if solr_spellcheck['suggestions'] && solr_spellcheck['suggestions'].length > 2
           collation = terms.join(" ").dup if terms
-      
+
           # If we are given a query string, tokenize it and strictly replace
           # the terms that aren't present in the index
-          if terms.present?
+          if terms.length > 0
             terms.each do |term|
               if (spellcheck_suggestions[term]||{})['origFreq'] == 0
                 collation[term] = spellcheck_suggestion_for(term)
               end
             end
           end
-      
+
           # If no query was given, or all terms are present in the index,
           # return Solr's suggested collation.
           if terms.length == 0
             collation = solr_spellcheck['suggestions'][-1]
           end
-        
+
           collation
         else
           nil
         end
       end
-      
+
       private
-  
+
       def dsl
         DSL::Search.new(self, @setup)
       end
--- sunspot/spec/api/query/standard_spec.rb
+++ sunspot/spec/api/query/standard_spec.rb
@@ -13,6 +13,7 @@ describe 'standard query', :type => :query do
   it_should_behave_like "geohash query"
   it_should_behave_like "spatial query"
   it_should_behave_like "stats query"
+  it_should_behave_like "spellcheck query"
 
   it 'adds a no-op query to :q parameter when no :q provided' do
     session.search Post do
