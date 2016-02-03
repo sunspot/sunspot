1ec60de995d961d99986930c136ccaba25b6c7e7
Paul Gideon Dann <pdgiddie@gmail.com>
2016-02-03 16:10:44 +0000

    Make rake tasks work with Rails 4

--- sunspot_solr/lib/sunspot_solr.rb
+++ sunspot_solr/lib/sunspot_solr.rb
@@ -1,5 +1,5 @@
 require 'sunspot/solr/server'
 
-if defined?(Rails) && Rails::VERSION::MAJOR == 3
+if defined?(Rails) && (3..4).include?(Rails::VERSION::MAJOR)
   require 'sunspot/solr/railtie'
 end
