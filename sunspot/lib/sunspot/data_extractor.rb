4be5c788df3189b4957185a285d1bf8e2f7ecb36
Alexander <aheaven87@gmail.com>
2018-03-01 13:44:33 +0200

    Add filter to DataExtractor that removes some of control chars that XML transport doesn't support.
    Fixes XML parser error in Solr
    org.apache.solr.common.SolrException: Illegal character ((CTRL-CHAR, code
    11))
    
    Relevant issues: sunspot#570

--- sunspot/lib/sunspot/data_extractor.rb
+++ sunspot/lib/sunspot/data_extractor.rb
@@ -5,16 +5,48 @@ module Sunspot
   # method, which takes an object and returns the value extracted from it.
   #
   module DataExtractor #:nodoc: all
+    #
+    # Abstract extractor to perform common actions on extracted values
+    #
+    class AbstractExtractor
+      CHAR_BLACKLIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 127]
+
+      def value_for(object)
+        extract_value_from(object)
+      end
+
+      private
+
+      def extract_value_from(object)
+        case object
+          when String
+            remove_blacklisted_chars(object)
+          when Array
+            object.map { |o| extract_value_from(o) }
+          when Hash
+            object.map { |(k, v)| [extract_value_from(k), extract_value_from(v)] }.to_h
+          else
+            object
+        end
+      end
+
+      def remove_blacklisted_chars(object)
+        object.chars.
+          reject { |c| CHAR_BLACKLIST.include?(c.ord) }.join
+      end
+    end
+
     # 
     # AttributeExtractors extract data by simply calling a method on the block.
     #
-    class AttributeExtractor
+    class AttributeExtractor < AbstractExtractor
       def initialize(attribute_name)
         @attribute_name = attribute_name
       end
 
       def value_for(object)
-        object.send(@attribute_name)
+        super object.send(@attribute_name)
       end
     end
 
@@ -24,26 +56,26 @@ module Sunspot
     # as the argument to the block. Either way, the return value of the block is
     # the value returned by the extractor.
     #
-    class BlockExtractor
+    class BlockExtractor < AbstractExtractor
       def initialize(&block)
         @block = block
       end
 
       def value_for(object)
-        Util.instance_eval_or_call(object, &@block)
+        super Util.instance_eval_or_call(object, &@block)
       end
     end
 
     # 
     # Constant data extractors simply return the same value for every object.
     #
-    class Constant
+    class Constant < AbstractExtractor
       def initialize(value)
         @value = value
       end
 
       def value_for(object)
-        @value
+        super @value
       end
     end
   end
