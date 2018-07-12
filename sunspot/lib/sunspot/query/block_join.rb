8ae39809baf843e958f1ee6bae167e0bf21614fd
Danilo Cianfrone <cianfrone@extendi.it>
2018-07-12 14:36:43 +0200

    added Sunspot::Query::BlockJoin

---
+++ sunspot/lib/sunspot/query/block_join.rb
@@ -0,0 +1,39 @@
+module Sunspot
+  module Query #:nodoc:
+    # TODO(ar3s3ru): add documentation
+    module BlockJoin
+      class ChildOf < Abstract
+        alias add_some_parents_filter add_secondary_filter
+
+        def to_params
+          { q: "{!child of=\"#{all_parents_filters}\"}#{secondary_filter}" }
+        end
+      end
+
+      class ParentWhich < Abstract
+        alias add_some_children_filter add_secondary_filter
+
+        def to_params
+          { q: "{!parent which=\"#{all_parents_filters}\"}#{secondary_filter}" }
+        end
+      end
+
+      private
+
+      class Abstract
+        DEFAULT_PARENTS_FILTER = '*.*'.freeze
+
+        attr_reader :all_parents_filters
+        attr_reader :secondary_filter
+
+        def initialize(parents_filters)
+          @all_parents_filters = parents_filters | DEFAULT_PARENTS_FILTER
+        end
+
+        def add_secondary_filter(filter)
+          @secondary_filter = filter
+        end
+      end
+    end
+  end
+end
