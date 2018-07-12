cb00e7fcbb2d1362d5c5e265598bae9f2c51affb
Danilo Cianfrone <cianfrone@extendi.it>
2018-07-12 10:59:55 +0200

    added cases to the child docs spec

--- sunspot/spec/api/indexer/child_documents_spec.rb
+++ sunspot/spec/api/indexer/child_documents_spec.rb
@@ -2,9 +2,12 @@ require File.expand_path('spec_helper', File.dirname(__FILE__))
 
 describe 'indexing child documents fields', :type => :indexer do
   it 'should index both parent and children' do
-    children = Array.new(3) { Child.new }
+    children = Array.new(3) { |i| Child.new(name: "Child #{i}") }
     parent   = Parent.new(name: 'Test Parent', children: children)
     Sunspot.index!(parent)
     expect(Sunspot.search(Parent) { with :name, parent.name }.results).to be_one
+    children.each do |child|
+      expect(Sunspot.search(Child) { with :name, child.name }.results).to be_one
+    end
   end
 end
