e6f5dbb8b487c4a5e6bc31e1977d09911f379227
Danilo Cianfrone <cianfrone@extendi.it>
2018-07-31 13:23:40 +0200

    fixed facet.date deprecation in Solr 6+

--- sunspot/lib/sunspot/search/date_facet.rb
+++ sunspot/lib/sunspot/search/date_facet.rb
@@ -12,16 +12,20 @@ module Sunspot
       def rows
         @rows ||=
           begin
-            data = @search.facet_response['facet_dates'][@field.indexed_name]
+            data = @search.facet_response['facet_ranges'][@field.indexed_name]
             gap = (@options[:time_interval] || 86400).to_i
             rows = []
-            data.each_pair do |value, count|
-              if value =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
-                start_time = @field.cast(value)
-                end_time = start_time + gap
-                rows << FacetRow.new(start_time..end_time, count, self)
+
+            if data['counts']
+              Hash[*data['counts']].each_pair do |value, count|
+                if value =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
+                  start_time = @field.cast(value)
+                  end_time = start_time + gap
+                  rows << FacetRow.new(start_time..end_time, count, self)
+                end
               end
             end
+
             if @options[:sort] == :count
               rows.sort! { |lrow, rrow| rrow.count <=> lrow.count }
             else
