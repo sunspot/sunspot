e1ef5e93bb45870fe5780a03a95e3fffb0cc4f4c
Gian Lorenzo Meocci <meocci@extendi.it>
2018-11-27 16:50:08 +0100

    update README

--- sunspot/spec/api/session_proxy/tbc_proxy_spec.rb
+++ sunspot/spec/api/session_proxy/tbc_proxy_spec.rb
@@ -4,11 +4,14 @@ class TbcPost < Post
 end
 
 describe Sunspot::SessionProxy::TbcSessionProxy do
-  # before :each do
-  #   Sunspot.session = Sunspot::SessionProxy::TbcSessionProxy.new
-  # end
+  return unless ENV["SOLR_MODE"] == "cloud"
 
-  # it "simple indexing" do
-  #   Sunspot.index(TbcPost.new(title: 'basic post'))
-  # end
+  before :each do
+    Sunspot.session = Sunspot::SessionProxy::TbcSessionProxy.new
+  end
+
+  it "simple indexing" do
+    Sunspot.index(TbcPost.new(title: 'basic post'))
+  end
+  
 end
