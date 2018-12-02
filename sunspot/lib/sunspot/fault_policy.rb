ef77b5f85974c149bef1bd9a5c7f77a0933e30c7
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-12-02 15:48:46 +0100

    refactoring

---
+++ sunspot/lib/sunspot/fault_policy.rb
@@ -0,0 +1,109 @@
+require 'logger'
+
+module Sunspot
+
+  #
+  # Implement Fault Policy:
+  # Takes server using a Round Robin policy from the current live nodes
+  #
+  module FaultPolicy
+    attr_reader :host_index, :current_hostname
+
+    #
+    # Get hostname (using RR policy)
+    #
+    def take_hostname
+      # takes all the configured nodes + that one that are derived by solr live config
+      hostnames = (@solr.live_nodes + seed_hosts)
+                  .uniq
+                  .reject { |h| is_faulty(h) }
+                  .sort
+
+      # round robin policy
+      # hostname format: <ip|hostname> | <ip|hostname>:<port>
+      @current_hostname = hostnames[@host_index]
+      current_host = @current_hostname.split(':')
+      @host_index = (@host_index + 1) % hostnames.size
+      if current_host.size == 2
+        [current_host.first, current_host.last.to_i]
+      else
+        current_host + [@config.port]
+      end
+    end
+
+    #
+    # Return true if an host is in fault state
+    # An host is in fault state if and only if:
+    # - #number of fault >= 3
+    # - time in fault state is >= 1h
+    #
+    def is_faulty(hostname)
+      @faulty_hosts.key?(hostname) &&
+        @faulty_hosts[hostname].first >= 3 &&
+        (Time.now - @faulty_hosts[hostname].last).to_i >= 3600
+    end
+
+    def reset_counter_faulty(hostname)
+      @faulty_hosts.delete(hostname)
+    end
+
+    def update_faulty_host(hostname)
+      @faulty_hosts[hostname]   ||= [0, Time.now]
+      @faulty_hosts[hostname][0] += 1
+      @faulty_hosts[hostname][1]  = Time.now
+
+      if is_faulty(hostname)
+        logger.error "Putting #{hostname} in fault state"
+      end
+    end
+
+    def seed_hosts
+      # uniform seed host
+      @seed_hosts ||= @config.hostnames.map do |h|
+        h = h.split(':')
+        if h.size == 2
+          "#{h.first}:#{h.last.to_i}"
+        else
+          "#{h.first}:#{@config.port}"
+        end
+      end
+    end
+
+    #
+    # Wrap the solr call and retries in case of ConnectionRefused or Http errors
+    #
+    def with_exception_handling
+      retries = 0
+      max_retries = 3
+      begin
+        yield
+        # reset counter of faulty_host for the current host
+        reset_counter_faulty(@current_hostname)
+      rescue RSolr::Error::ConnectionRefused, RSolr::Error::Http => e
+        logger.error "Error connecting to Solr #{e.message}"
+
+        # update the map of faulty hosts
+        update_faulty_host(@current_hostname)
+
+        if retries < max_retries
+          retries += 1
+          sleep_for = 2**retries
+          logger.error "Retrying Solr connection in #{sleep_for} seconds... (#{retries} of #{max_retries})"
+          sleep(sleep_for)
+          retry
+        else
+          logger.error 'Reached max Solr connection retry count.'
+          raise e
+        end
+      end
+    rescue StandardError => e
+      logger.error "Exception: #{e.inspect}"
+      raise e
+    end
+
+    def logger
+      @logger ||= ::Rails.logger
+      @logger ||= Logger.new(STDOUT)
+    end
+  end
+end
--- sunspot/lib/sunspot/session_proxy/tbc_session_proxy.rb
+++ sunspot/lib/sunspot/session_proxy/tbc_session_proxy.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
-require 'logger'
+require '../fault_policy'
 
 module Sunspot
   module SessionProxy
@@ -37,6 +37,8 @@ module Sunspot
                     :atomic_update!, :remove_all, :remove_all!
 
       attr_reader :solr, :config
+
+      include FaultPolicy
 
       def initialize(
         config: Sunspot::Configuration.build,
@@ -275,38 +277,6 @@ module Sunspot
 
       private
 
-      #
-      # Wrap the solr call and retries in case of ConnectionRefused or Http errors
-      #
-      def with_exception_handling
-        retries = 0
-        max_retries = 3
-        begin
-          yield
-          # reset counter of faulty_host for the current host
-          reset_counter_faulty(@current_hostname)
-        rescue RSolr::Error::ConnectionRefused, RSolr::Error::Http => e
-          logger.error "Error connecting to Solr #{e.message}"
-
-          # update the map of faulty hosts
-          update_faulty_host(@current_hostname)
-
-          if retries < max_retries
-            retries += 1
-            sleep_for = 2**retries
-            logger.error "Retrying Solr connection in #{sleep_for} seconds... (#{retries} of #{max_retries})"
-            sleep(sleep_for)
-            retry
-          else
-            logger.error 'Reached max Solr connection retry count.'
-            raise e
-          end
-        end
-      rescue StandardError => e
-        logger.error "Exception: #{e.inspect}"
-        raise e
-      end
-
       def calculate_search_collections(date_from:, date_to:)
         date_from = Time.at(date_from).utc.to_date
         date_to = Time.at(date_to).utc.to_date
@@ -355,66 +325,6 @@ module Sunspot
       end
 
       #
-      # Get hostname (using RR policy)
-      #
-      def take_hostname
-        # takes all the configured nodes + that one that are derived by solr live config
-        hostnames = (@solr.live_nodes + seed_hosts)
-                    .uniq
-                    .reject { |h| is_faulty(h) }
-                    .sort
-
-        # round robin policy
-        # hostname format: <ip|hostname> | <ip|hostname>:<port>
-        @current_hostname = hostnames[@host_index]
-        current_host = @current_hostname.split(':')
-        @host_index = (@host_index + 1) % hostnames.size
-        if current_host.size == 2
-          [current_host.first, current_host.last.to_i]
-        else
-          current_host + [@config.port]
-        end
-      end
-
-      #
-      # Return true if an host is in fault state
-      # An host is in fault state if and only if:
-      # - #number of fault >= 3
-      # - time in fault state is >= 1h
-      #
-      def is_faulty(hostname)
-        @faulty_hosts.key?(hostname) &&
-          @faulty_hosts[hostname].first >= 3 &&
-          (Time.now - @faulty_hosts[hostname].last).to_i >= 3600
-      end
-
-      def reset_counter_faulty(hostname)
-        @faulty_hosts.delete(hostname)
-      end
-
-      def update_faulty_host(hostname)
-        @faulty_hosts[hostname]   ||= [0, Time.now]
-        @faulty_hosts[hostname][0] += 1
-        @faulty_hosts[hostname][1]  = Time.now
-
-        if is_faulty(hostname)
-          logger.error "Putting #{hostname} in fault state"
-        end
-      end
-
-      def seed_hosts
-        # uniform seed host
-        @seed_hosts ||= @config.hostnames.map do |h|
-          h = h.split(':')
-          if h.size == 2
-            "#{h.first}:#{h.last.to_i}"
-          else
-            "#{h.first}:#{@config.port}"
-          end
-        end
-      end
-
-      #
       # Session generator
       #
       def gen_session(path)
@@ -429,11 +339,6 @@ module Sunspot
         c.solr.open_timeout = @config.open_timeout
         c.solr.proxy = @config.proxy
         Session.new(c)
-      end
-
-      def logger
-        @logger ||= ::Rails.logger
-        @logger ||= Logger.new(STDOUT)
       end
     end
   end
