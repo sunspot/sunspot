dcf7dab17150a069d8e696a3dec01bdaa2cb6965
Sergey Glukhov <sergey.glukhov@gmail.com>
2019-07-17 10:52:46 +0300

    better spec names for atomic updates

--- sunspot/spec/integration/atomic_updates_spec.rb
+++ sunspot/spec/integration/atomic_updates_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
-describe 'atomic updates' do
+describe 'Atomic Update feature' do
   before :all do
     Sunspot.remove_all
   end
@@ -18,7 +18,7 @@ describe 'atomic updates' do
     hit
   end
 
-  it 'should update single record fields one by one' do
+  it 'updates single record fields one by one' do
     post = Post.new(title: 'A Title', featured: true)
     Sunspot.index!(post)
     
@@ -31,7 +31,7 @@ describe 'atomic updates' do
     validate_hit(find_indexed_post(post.id), title: 'A New Title', featured: false)
   end
 
-  it 'should update fields for multiple records' do
+  it 'updates fields for multiple records' do
     post1 = Post.new(title: 'A First Title', featured: true)
     post2 = Post.new(title: 'A Second Title', featured: false)
     Sunspot.index!(post1, post2)
@@ -44,7 +44,7 @@ describe 'atomic updates' do
     validate_hit(find_indexed_post(post2.id), title: 'A Second Title', featured: true)
   end
 
-  it 'should clear field value properly' do
+  it 'clears field value properly' do
     post = Post.new(title: 'A Title', tags: %w(tag1 tag2), featured: true)
     Sunspot.index!(post)
     validate_hit(find_indexed_post(post.id), title: post.title, tag_list: post.tags, featured: true)
