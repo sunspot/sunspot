98369f1abbfb322c910e1c49e1552b2b634ed4c9
Rubén González <2051199+rbngzlv@users.noreply.github.com>
2020-03-26 18:17:59 +0100

    Add failing test for autoloading engines

--- sunspot_rails/spec/rails_app/config/application.rb
+++ sunspot_rails/spec/rails_app/config/application.rb
@@ -4,6 +4,9 @@ require 'rails/all'
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
+# Load the test engine
+require File.expand_path('../../vendor/engines/test_engine/lib/test_engine', __FILE__)
+
 module RailsApp
   class Application < Rails::Application
     config.encoding = 'utf-8'
---
+++ sunspot_rails/spec/rails_app/vendor/engines/test_engine/app/models/test_engine/rake_task_auto_load_test_model.rb
@@ -0,0 +1,15 @@
+# This model should not be used for any test other than the spec test that
+# checks if all models are loaded. We don't want to pre-load this model in
+# another test because we're checking to see if it will be auto-loaded by
+# the reindex task
+module TestEngine
+  class RakeTaskAutoLoadTestModel < ActiveRecord::Base
+    def self.table_name
+      'posts'
+    end
+
+    searchable do
+      string :name
+    end
+  end
+end
