533089efed6dd338c4d4aed207da1233502c85d0
Alexander S <heaven@users.noreply.github.com>
2020-07-27 12:12:25 +0300

    Restriction queries with joined fields have to be in double quotes (#977)

--- sunspot/spec/api/query/join_spec.rb
+++ sunspot/spec/api/query/join_spec.rb
@@ -6,7 +6,7 @@ describe 'join' do
       with(:caption, 'blah')
     end
     expect(connection).to have_last_search_including(
-      :fq, "{!join from=photo_container_id_i to=id_i v='type:\"Photo\" AND caption_s:blah'}")
+      :fq, "{!join from=photo_container_id_i to=id_i v='type:\"Photo\" AND caption_s:\"blah\"'}")
   end
 
   it 'should greater_than search by join' do
