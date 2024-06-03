51913ffa1d524bda77f2d56ea706455624ec87ed
Mark Young <mark.young@atg.auto>
2024-06-03 13:57:53 +0100

    Revert "Fix rails 7.1 deprecation warning"
    This reverts commit ed61f699229e409f96405761c47d09968eedfa61.

--- sunspot_rails/lib/sunspot/rails/log_subscriber.rb
+++ sunspot_rails/lib/sunspot/rails/log_subscriber.rb
@@ -33,15 +33,15 @@ module Sunspot
         name = '%s (%.1fms)' % ["SOLR Request", event.duration]
 
         # produces: path=select parameters={fq: ["type:Tag"], q: "rossi", fl: "* score", qf: "tag_name_text", defType: "edismax", start: 0, rows: 20}
-        path = color(event.payload[:path], nil, bold: true)
+        path = color(event.payload[:path], BOLD, true)
         parameters = event.payload[:parameters].map { |k, v|
           v = "\"#{v}\"" if v.is_a? String
           v = v.to_s.gsub(/\\/,'') # unescape
-          "#{k}: #{color(v, nil, bold: true)}"
+          "#{k}: #{color(v, BOLD, true)}"
         }.join(', ')
         request = "path=#{path} parameters={#{parameters}}"
 
-        debug "  #{color(name, GREEN, bold: true)}  [ #{request} ]"
+        debug "  #{color(name, GREEN, true)}  [ #{request} ]"
       end
     end
   end
